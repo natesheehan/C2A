94268733c832a6f50eb9f315a92b068a1b65e772
Sheehan <ns651@exeter.ac.uk>
2023-04-28 01:52:47 +0100

    update data-raw

--- code/usage-network.r
+++ code/usage-network.r
@@ -12,14 +12,14 @@
 source("build.r")
 # Build Script ------------------------------------------------------------
 # 1. Convert Dimensions biblometric CSV 2 Dataframe -----------------------
-if (file.exists("publications/NCBI/nbi.rds")) {
-  nbi = readRDS("publications/NCBI/nbi.rds")
-  gisaid = readRDS("publications/GISAID/gisaid.rds")
-  ena = readRDS("publications/CV19DP/ena.rds")
+if (file.exists("data-raw/publications/NCBI/nbi.rds")) {
+  nbi = readRDS("data-raw/publications/NCBI/nbi.rds")
+  gisaid = readRDS("data-raw/publications/GISAID/gisaid.rds")
+  ena = readRDS("data-raw/publications/CV19DP/ena.rds")
 } else {
-  nbi = convert_biblo("publications/Dimensions-Publication-2023-02-16_00-37-11.csv")
-  gisaid = convert_biblo("publications/Dimensions-Publication-2023-02-16_00-40-25.csv")
-  ena = convert_biblo("publications/Dimensions-Publication-2023-02-16_00-40-09.csv")
+  nbi = convert_biblo("data-raw/publications/Dimensions-Publication-2023-02-16_00-37-11.csv")
+  gisaid = convert_biblo("data-raw/publications/Dimensions-Publication-2023-02-16_00-40-25.csv")
+  ena = convert_biblo("data-raw/publications/Dimensions-Publication-2023-02-16_00-40-09.csv")
 }
 
 # 2. Filter Data ----------------------------------------------------------
@@ -42,26 +42,26 @@ ena_res = biblio_analysis(ena_clean)
 plot_ss_network(nbi_clean,gisaid_clean,ena_clean)
 
 # 4. Build Networks
-calculate_networks(nbi_clean, "publications/NCBI/")
-calculate_networks(gisaid_clean, "publications/GISAID/")
-calculate_networks(ena_clean, "publications/CV19DP/")
+calculate_networks(nbi_clean, "data-raw/publications/NCBI/")
+calculate_networks(gisaid_clean, "data-raw/publications/GISAID/")
+calculate_networks(ena_clean, "data-raw/publications/CV19DP/")
+
+# 5. Build Network
+a = plot_network(network = readRDS("data-raw/publications/GISAID/funding_country_net.rds"), type = "funding_country", di = "GISAID") # 303.4
+b = plot_network(network = readRDS("data-raw/publications/GISAID/country_net.rds"), type = "country", di = "GISAID") # 157.5
+c = plot_network(network = readRDS("data-raw/publications/GISAID/institution_net.rds"), type = "institution", di = "GISAID") #7
+d = plot_network(network = readRDS("data-raw/publications/GISAID/city_net.rds"), type = "city", di = "GISAID") #22
 
 
-a = plot_network(network = readRDS("publications/GISAID/funding_country_net.rds"), type = "funding_country", di = "GISAID") # 303.4
-b = plot_network(network = readRDS("publications/GISAID/country_net.rds"), type = "country", di = "GISAID") # 157.5
-c = plot_network(network = readRDS("publications/GISAID/institution_net.rds"), type = "institution", di = "GISAID") #7
-d = plot_network(network = readRDS("publications/GISAID/city_net.rds"), type = "city", di = "GISAID") #22
-
-
-e = plot_network(network = readRDS("publications/CV19DP/funding_country_net.rds"), type = "funding_country", di = "CV19DP") #153
-f = plot_network(network = readRDS("publications/CV19DP/country_net.rds"), "country", "CV19DP") # 118.92
-g = plot_network(network = readRDS("publications/CV19DP/institution_net.rds"), type = "institution", di = "CV19DP") #2
-h = plot_network(network = readRDS("publications/CV19DP/city_net.rds"), type = "city", di = "CV19DP") # 12
+e = plot_network(network = readRDS("data-raw/publications/CV19DP/funding_country_net.rds"), type = "funding_country", di = "CV19DP") #153
+f = plot_network(network = readRDS("data-raw/publications/CV19DP/country_net.rds"), "country", "CV19DP") # 118.92
+g = plot_network(network = readRDS("data-raw/publications/CV19DP/institution_net.rds"), type = "institution", di = "CV19DP") #2
+h = plot_network(network = readRDS("data-raw/publications/CV19DP/city_net.rds"), type = "city", di = "CV19DP") # 12
 
 ggarrange(a,e,b,f,c,g,d,h,nrow = 4,ncol = 2)
 
 ggsave(
-  paste0("publications/network-plot.png"),
+  paste0("data-raw/publications/network-plot.png"),
   dpi = 320,
   width = 22,
   height = 22,
